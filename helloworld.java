8c688c76d392968c5cb44c10ae1a62940cbd43ae
Akarshjha03 <akarshjha.matrix@gmail.com>
2024-06-18 23:02:52 +0530

    Updating the code

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,14 @@
-public class helloworld {
+//This is the first program of java which shows different ways to print output
+public class Helloworld {
 	public static void main(String args[])
 	{
+		//after printing this the cursor will move to next line
 		System.out.println("Hello World");
+
+		//here, after printing the values curssor doesn't moves to next line
+		System.out.print("null");
+
+		//normal way of getting the cursor to nextline
+		System.out.print("Hello world\n");
 	}
 }
