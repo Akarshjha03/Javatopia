edecb798699671dc0f04024ffe821e11a3e0c716
Akarshjha03 <akarshjha.matrix@gmail.com>
2024-06-19 09:16:48 +0530

    Adding new code

---
+++ Patterns.java
@@ -0,0 +1,7 @@
+//This is basic patterns printing using output functions
+
+public class Patterns {
+    public static void main(String args[]) {
+        System.out.println("*\n**\n***");
+    }
+}
