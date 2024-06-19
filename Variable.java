aa18f5a1e07b91f516a344cb8ac8eac3b955301d
Akarshjha03 <akarshjha.matrix@gmail.com>
2024-06-19 09:54:25 +0530

    Adding new code

---
+++ Variable.java
@@ -0,0 +1,16 @@
+public class Variable {
+    public static void main(String args[]) {
+        int x = 10; // integer variable
+        double y = 20.5; // double variable
+        char z = 'A'; // character variable
+        int a = 30;
+
+        int sum = x+a;
+        int difference = a-x;
+        int product = x*a;
+
+        System.out.println("the sum is:"+ sum);
+        System.out.println("the difference is:"+ difference);
+        System.out.println("the product is:"+ product);
+    }
+}
