1948f48203f65d36f00495fe97af52657c376a75
Akarshjha03 <akarshjha.matrix@gmail.com>
2024-06-19 10:50:52 +0530

    Adding new code

---
+++ input1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class input1 {
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        Scanner sca = new Scanner (System.in);
+        int b = sca.nextInt();
+
+        int sum = a+b;
+
+        System.out.println("The sum is:"+sum);
+    }
+}
