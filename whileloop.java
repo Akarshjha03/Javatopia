2aad2bc9a9c01fb6ac8a6f1203ea7e10ad3b3e79
Akarshjha03 <akarshjha.matrix@gmail.com>
2024-06-23 14:39:18 +0530

    Adding new code

---
+++ whileloop.java
@@ -0,0 +1,10 @@
+//This code uses while loop for printing numbers 1-10
+public class whileloop {
+    public static void main(String args[]){
+        int i = 1;
+        while(i<11){
+            System.out.println("The count is:" +i);
+            i++;
+        }
+    }
+}
