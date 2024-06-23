bf9334ac65f89d36a17dfafbff2577e8068c3765
Akarshjha03 <akarshjha.matrix@gmail.com>
2024-06-23 14:43:05 +0530

    Adding new code

---
+++ dowhileloop.java
@@ -0,0 +1,11 @@
+//This code uses do-while loop for printing numbers from 1-10 
+
+public class dowhileloop {
+    public static void main(String args[]){
+        int i = 1;
+        do {
+            System.out.println("The count is:"+i);
+            i++;
+        }while(i<11);
+    }
+}
