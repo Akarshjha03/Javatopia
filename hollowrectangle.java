e88fe79c168b8346753f402d1e63ab3bb0019905
Akarshjha03 <akarshjha.matrix@gmail.com>
2024-06-24 23:02:55 +0530

    Adding new code

---
+++ hollowrectangle.java
@@ -0,0 +1,18 @@
+public class hollowrectangle {
+    public static void main (String args[]){
+        int n = 4;
+        int m = 5;
+
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=m; j++){
+                if(i == 1 || i == n || j == 1 || j == m){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
