d84971f58e6a8b7b07a730b8ecd83349541f52f4
Akarshjha03 <akarshjha.matrix@gmail.com>
2024-06-24 18:58:11 +0530

    Adding new code

---
+++ solidrectangle.java
@@ -0,0 +1,12 @@
+//This code prints a solid rectangle using nested for loop
+
+public class solidrectangle{
+    public static void main(String args[]){
+        for(int i = 1; i<=7; i++){
+            for(int j = 1; j<=6; j++){
+                System.out.print("*");
+            }
+            System.out.println("*");
+        }
+    }
+}
