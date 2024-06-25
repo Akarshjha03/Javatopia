203b9f56c7817531e3a657b5a578298d81c9f289
Akarshjha03 <akarshjha.matrix@gmail.com>
2024-06-25 18:48:26 +0530

    Adding new code

--- invertedhpyramid.java
+++ invertedhpyramid.java
@@ -1,10 +1,8 @@
-//This code prints  the half pyramid pattern
-
-public class halfpyramid {
+public class invertedhpyramid {
     public static void main(String args[]){
         int n = 5;
-        for(int i =1 ; i<=n ; i++){
-            for(int j = 1 ; j<=i ; j++){
+        for(int i = n; i >= 1; i--){
+            for(int j = 1; j <= i; j++){
                 System.out.print("* ");
             }
             System.out.println();
