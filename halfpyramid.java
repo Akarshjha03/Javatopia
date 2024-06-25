3f87901a0232ea3e79e300bfd755f673cb421b83
Akarshjha03 <akarshjha.matrix@gmail.com>
2024-06-25 18:37:48 +0530

    Adding new code

---
+++ halfpyramid.java
@@ -0,0 +1,11 @@
+public class halfpyramid {
+    public static void main(String args[]){
+        int n = 5;
+        for(int i =1 ; i<=n ; i++){
+            for(int j = 1 ; j<=i ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
