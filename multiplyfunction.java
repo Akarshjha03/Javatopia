dea46ecb466ffe404732e3320084e2708c65b5cb
Akarshjha03 <akarshjha.matrix@gmail.com>
2024-06-26 10:35:08 +0530

    Adding new code

---
+++ multiplyfunction.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class multiplyfunction {
+    public static int CalculateProduct(int a,int b){
+        int product = a * b;
+        return product;
+    }
+    public static void main(String[] args){
+        System.out.println("Enter the value of a & b:");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int product = CalculateProduct(a,b); //calling the function and passing the the parameters
+        System.out.println("The product of a & b is:"+product);
+    }
+}
