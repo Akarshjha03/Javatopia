46c399873d82843df3cc8fbd0d1c1d7b43e37e04
Akarshjha03 <akarshjha.matrix@gmail.com>
2024-07-03 10:45:11 +0530

    Adding array code

---
+++ Array.java
@@ -0,0 +1,12 @@
+//This code implements basic array
+public class Array {
+    public static void main(String args[]){
+        int[] marks = new int [3];
+        marks[0] = 10;
+        marks[1] = 20;
+        marks[2] = 30;
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+    }
+}
