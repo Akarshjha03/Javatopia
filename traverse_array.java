cb58f4c6d1217cfe3676ca41758df46d8c471e0a
Akarshjha03 <akarshjha.matrix@gmail.com>
2024-07-03 10:53:40 +0530

    Add traverse_array.java

---
+++ traverse_array.java
@@ -0,0 +1,19 @@
+public class traverse_array {
+    public static void main (String args[]){
+        int arr[] = new int [10];
+        arr[0]=1;
+        arr[1]=2;
+        arr[2]=3;
+        arr[3]=4;
+        arr[4]=5;
+        arr[5]=6;
+        arr[6]=7;
+        arr[7]=8;
+        arr[8]=9;
+        arr[9]=10;
+        for (int i=0;i<10;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
+
