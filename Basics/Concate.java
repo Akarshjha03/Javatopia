b6eb4209eac243746c7466569c1a8a984044325b
Akarshjha03 <akarshjha.matrix@gmail.com>
2024-07-14 12:57:31 +0530

    Add concate file

---
+++ Basics/Concate.java
@@ -0,0 +1,10 @@
+public class Concate {
+    public static void main(String[] args) {
+        String s1 = "AKARSH";
+        String s2 = "JHA";
+        String s3 = s1.concat(s2);
+        String s4 = s1 + " " + s2;
+        System.out.println(s3);
+        System.out.println(s4);
+    }
+}
