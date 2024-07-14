b2ebfa51f815821bcc6fa6c5d12bdc8ee15258c7
Akarshjha03 <akarshjha.matrix@gmail.com>
2024-07-14 13:06:53 +0530

    Adding charat() file

---
+++ Basics/Characterat.java
@@ -0,0 +1,10 @@
+public class Characterat {
+    public static void main(String[] args) { //printing character at index
+        String Name = "AKARSH JHA";
+        for (int i =0; i<Name.length(); i++){
+            System.out.println(Name.charAt(i));
+
+        //System.out.println(Name.CharAt(3)); Run this instead of for loop
+        }
+    }
+}
