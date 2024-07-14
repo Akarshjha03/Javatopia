3e310e0a4f3dd69b521bef7f8ce078c14eae8ca3
Akarshjha03 <akarshjha.matrix@gmail.com>
2024-07-14 13:17:43 +0530

    Adding compareto() file

---
+++ Basics/Compareto.java
@@ -0,0 +1,13 @@
+public class Compareto {
+    public static void main(String[] args) { //This code uses compareto() predefined function
+        String Fname = "Akarsh";
+        String Lname = "Jha";
+
+        if(Fname.compareTo(Lname)==0){
+            System.out.println("Strings are equal");
+        }
+        else {
+            System.out.println("Strings are not equal");
+        }
+    }
+}
